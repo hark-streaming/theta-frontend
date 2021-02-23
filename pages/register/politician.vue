ae24152e6bbbfd224679dc731e23ae37a6b8d200
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-02-22 22:35:35 -0800

    set up subregister page layouts

---
+++ pages/register/politician.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-container class="px-16">
+        <div class="mb-4 d-flex justify-space-around">POLITICIAN REGISTER PAGE</div>
+
+        <!-- 3 card layout -->
+        <v-row>
+            <v-col cols="4">
+                <v-card elevation="2">
+                    <v-card-title> Steps </v-card-title>
+                </v-card>
+            </v-col>
+            <v-col cols="8">
+                <v-card elevation="2">
+                    <v-card-title> Form </v-card-title>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import { auth } from "@/plugins/firebase.js";
+
+export default {
+    name: "register",
+
+    data() {
+        return {};
+    },
+
+    methods: {},
+
+    computed: {},
+
+    mounted() {},
+
+    beforeDestroy() {},
+};
+</script>
