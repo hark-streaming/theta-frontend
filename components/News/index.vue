e2a193ec5f7de54c918e740bcf34d1c4572a681a
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-02-27 20:20:32 -0800

    added minnalousifornia to states

---
+++ components/News/index.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div class="my-4">
+    <v-img src="https://cdn.cnn.com/cnnnext/dam/assets/210226144237-03-cpac-day-1-cruz-2021-medium-tease.jpg" />
+    <div>IMAGE TITLE</div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'News Article'
+}
+</script>
+
+<style lang='scss'>
+
+</style>
--- pages/_countries/_state/index.vue
+++ pages/_countries/_state/index.vue
@@ -1,16 +1,48 @@
 <template>
-  <div>
-    state
-  </div>
+  <v-container>
+    <h1 class="mb-4 d-flex justify-space-around">MINELOUISIFORNIA</h1>
+
+    <v-row>
+      <!-- News & Stats -->
+      <v-col cols="4">
+        <img src="/images/minnalouisfornia.png" class="d-flex state-img" />
+        <v-card elevation="2" class="pa-4 sticky-card">
+          <v-card-title class="mt-2">News</v-card-title>
+          <news />
+          <news />
+          <news />
+        </v-card>
+      </v-col>
+
+      <!-- Streams & Videos -->
+      <v-col cols="8">
+        <v-card elevation="2" class="pa-4">
+          <!--
+                <stream-grid >
+
+                </stream-grid>
+                -->
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: "channel",
 
-  async mounted() {
-    
-  },
+  async mounted() {},
 };
 </script>
+
+<style>
+.state-img {
+  max-width: 100%;
+  max-height: 300px;
+  /*width: 50%;*/
+  margin-left: auto;
+  margin-right: auto;
+  height: auto;
+}
+</style>
