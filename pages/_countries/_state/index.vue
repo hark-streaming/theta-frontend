4792f0f3688a3559e3bcf12bc313e92e5b181cdf
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-02-27 20:51:43 -0800

    state news sticky

--- pages/_countries/_state/index.vue
+++ pages/_countries/_state/index.vue
@@ -4,10 +4,10 @@
 
     <v-row>
       <!-- News & Stats -->
-      <v-col cols="4">
+      <v-col cols="3" clipped left>
         <img src="/images/minnalouisfornia.png" class="d-flex state-img" />
-        <v-card elevation="2" class="pa-4 sticky-card">
-          <v-card-title class="mt-2">News</v-card-title>
+        <v-card elevation="2" class="pa-4 my-3 sticky-card">
+          <v-card-title class="mt-2">Minelouisifornia Articles</v-card-title>
           <news />
           <news />
           <news />
@@ -15,13 +15,14 @@
       </v-col>
 
       <!-- Streams & Videos -->
-      <v-col cols="8">
+      <v-col cols="9">
         <v-card elevation="2" class="pa-4">
           <!--
                 <stream-grid >
 
                 </stream-grid>
                 -->
+          <div class="brogermy" >AHHH</div>
         </v-card>
       </v-col>
     </v-row>
@@ -44,5 +45,14 @@ export default {
   margin-left: auto;
   margin-right: auto;
   height: auto;
+}
+
+.sticky-card {
+    position: sticky;
+    top: 60px;
+}
+
+.brogermy {
+  min-height: 2000px;
 }
 </style>
