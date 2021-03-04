ba8f2241b4f6d40f7a5da263a399a91eab29b463
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-03-03 19:44:59 -0800

    redirected country to home

--- pages/_countries/index.vue
+++ pages/_countries/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>country</div>
+
 </template>
 
 <script>
 export default {
   name: "channel",
 
-  async mounted() {},
+  async created() {
+    // we're only based in the usa right now, and we're not heading towards national news yet
+    // so for now, we're focusing on local areas (state & county/city)
+    this.$router.replace("/");
+  },
 };
 </script>
--- pages/channel/index.vue
+++ pages/channel/index.vue
@@ -7,7 +7,6 @@ export default {
   name: "channel",
 
   async created() {
-    console.log("AHHHHHHH");
     this.$router.replace("/");
   },
 };
