b1d427846629973858d9aa8786cb692aa693d186
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-03-10 22:24:19 -0800

    theta web widget works

--- components/ThetaPlayer/ThetaVideoPlayer.vue
+++ components/ThetaPlayer/ThetaVideoPlayer.vue
@@ -16,7 +16,7 @@
                 :poster="posterCacheBusted"
                 :style="{ width: '100%' }"
             ></video>
-
+            <!-- <div id="MY_THETA_WEB_WIDGET_ID"></div> -->
             <!-- Detached player topbar overlay -->
             <slot name="minioverlay">
                 <div v-if="docked" class="d-flex align-center detach-overlay">
@@ -53,7 +53,6 @@ import hls from "hls.js";
 // if(process.brower){
 //     import Theta from "../../static/js/theta.js"
 // }
-
 
 //import "@videojs/http-streaming";
 //import "videojs-contrib-quality-levels";
@@ -99,6 +98,24 @@ export default {
                         this.playerInitialize();
                     },
                 },
+                {  
+                    // hid: "thetawebwidget",
+                    // src: "https://theta-web-widgets.thetatoken.org/js/ThetaWebWidgets.js",
+                    // callback: () => {
+                    //     if (process.browser) {
+                    //         console.log("connecting the theta widget");
+
+                    //         //console.log("i made a new theta", theta);
+                    //         //this.createThetaObj("testman", "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8","12345")
+                    //         var widget = new ThetaWebWidgets.OverviewWithTrafficChartWidget();
+                    //         widget.setTheme(ThetaWebWidgets.Themes.Light);
+                    //         widget.setMainMessage("TEST TIME");
+                    //         widget.render("MY_THETA_WEB_WIDGET_ID");
+
+                    //         //console.log("the window event listeners", getEventListeners(window));
+                    //     }
+                    // },
+                },
             ],
         };
     },
@@ -133,13 +150,17 @@ export default {
         playerInitialize() {
             // This plugin call registers the stuff needed for the Theta videojs player
             console.log("CALLING ThetaPlayerSetup function");
-            //this.$ThetaPlayerSetup(window.Theta, hls, videojs);
+            // this.$ThetaPlayerSetup(this.createThetaObj("testman", "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8","12345")
+            // , hls, videojs);
+            
             this.$ThetaPlayerSetup(window.Theta, hls, videojs);
-            
-            console.log("this is window theta", window.Theta);
-            
 
-            console.log("INITIALIZING PLAYER"); 
+            //turn theta debugging on
+            window.Theta.setDebug(true);
+
+            console.log("this is window theta", window.Theta);
+
+            console.log("INITIALIZING PLAYER");
             console.log(
                 `URL: ${this.source.url}, TYPE: ${this.source.type}, POSTER: ${this.poster}, AUTOPLAY: ${this.autoplay}, POSTER: ${this.poster}`
             );
@@ -152,9 +173,9 @@ export default {
                 techOrder: ["theta_hlsjs" /*, "html5"*/], // disable html5 fallback so we know when theta broken
                 theta_hlsjs: {
                     videoId: this.streamer,
-                    // TODO: make sure firebase auth is loaded by this point 
+                    // TODO: make sure firebase auth is loaded by this point
                     //       so there is no accidental userId/guestId mismatch
-                    userId: "12345",//this.getUserId, 
+                    userId: this.getUserId,
                     walletUrl:
                         "wss://api-wallet-service.thetatoken.org/theta/ws",
                     onWalletAccessToken: this.getWalletAccessToken,
@@ -428,6 +449,94 @@ export default {
             });
         },
 
+        // createThetaObj(vid_id, vid_url, userId){
+        //     const PEER_SERVER_HOST = "prod-theta-peerjs.thetatoken.org";
+        //     const PEER_SERVER_PORT = 8700;
+        //     const TRACKER_SERVER_HOST = "prod-testnet-grouping.thetatoken.org";
+        //     const TRACKER_SERVER_PORT = 8700;
+
+        //     const PLATFORM_THETA_WALLET_SERVICE_URL =
+        //         "wss://api-wallet-service.thetatoken.org/theta/ws";
+
+        //     // TODO Fill these in with your data
+        //     const VIDEO_ID = vid_id;
+        //     //const VIDEO_URL = "https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8";
+        //     const VIDEO_URL = vid_url
+        //         "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+        //     let theta = new Theta({
+        //         //TODO adjust params as needed depending on your HLS settings
+        //         fragmentSize: 5000,
+        //         failoverFactor: 0.7,
+        //         fragmentTimeout: 3000,
+        //         probeTimeout: 600,
+        //         statsReportInterval: 90000,
+        //         peerReqInterval: 120000,
+
+        //         videoId: VIDEO_ID,
+        //         userId: userId,
+        //         wallet: null,
+
+        //         peerServer: {
+        //             host: PEER_SERVER_HOST,
+        //             port: PEER_SERVER_PORT,
+        //             secure: true,
+        //         },
+        //         trackerServer: {
+        //             host: TRACKER_SERVER_HOST,
+        //             port: TRACKER_SERVER_PORT,
+        //             secure: true,
+        //             path: "",
+        //         },
+
+        //         debug: true,
+        //     });
+
+        //     // Event handlers
+        //     theta.addEventListener(Theta.Events.PEERS_CHANGED, function (data) {
+        //         // Connected peers changed
+        //         // Data:
+        //         // totalPeers : Integer
+        //     });
+        //     theta.addEventListener(Theta.Events.TRAFFIC, function (data) {
+        //         // Bandwidth was used
+        //         // Data:
+        //         // type : String ('cdn', 'p2p_inbound', 'p2p_outbound')
+        //         // stats : Object
+        //         // stats.size : Integer - Total bytes
+        //     });
+        //     theta.addEventListener(
+        //         Theta.Events.PAYMENT_RECEIVED,
+        //         function (data) {
+        //             // Payment received
+        //             // Data:
+        //             // payment : Object - info about the payment
+        //             // payment.amount : Integer - Payment amount in GammaWei
+        //         }
+        //     );
+        //     theta.addEventListener(Theta.Events.PAYMENT_SENT, function (data) {
+        //         // Payment sent
+        //         // Data:
+        //         // payment : Object - info about the payment
+        //         // payment.amount : Integer - Payment amount in GammaWei
+        //     });
+        //     theta.addEventListener(
+        //         Theta.Events.ACCOUNT_UPDATED,
+        //         function (data) {
+        //             // Account/waller updated
+        //             // Data:
+        //             // account : Object - info about the account/wallet
+        //         }
+        //     );
+        //     theta.start();
+
+        //     //If you are using the Theta widget, connect the widget so it can listen to events
+        //     theta.connectWidget();
+
+        //     console.log("new theta created", theta);
+
+        //     return theta;
+        // },
+
         trackWatchTime() {
             if (this.player.paused() || !this.live) {
                 return;
@@ -576,18 +685,15 @@ export default {
             }
         },
 
-        getUserId(){
-            if(auth.currentUser == null){
+        getUserId() {
+            if (auth.currentUser == null) {
                 console.log("not logged in, using guest id for theta");
-                return "" + (new Date().getTime())
-            }
-            else {
+                return "" + new Date().getTime();
+            } else {
                 console.log("logged in, using uid for theta");
                 return auth.currentUser.uid;
             }
         },
-
-        
 
         ...mapMutations(Player.namespace, {
             setPiP: Player.$mutations.setPiP,
@@ -650,7 +756,7 @@ export default {
         //this.$hello('mounted');
 
         //this.playerInitialize();
-        
+
         // from https://stackoverflow.com/questions/43652265/how-to-run-vuejs-code-only-after-vue-is-fully-loaded-and-initialized/43656809
         // window.addEventListener('load', () => {
         //   console.log("LOAD EVENT FIRED, INITIALIZING PLAYER");
