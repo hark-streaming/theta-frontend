c059648aa15cfcaec06556fe52c3c052797e2cf0
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-03-21 18:08:52 -0700

    started donation card form

---
+++ components/DonationCards/CardForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-flex xs12 sm10 md8 lg6>
+        <v-card>
+            <v-flex class="mb-3">
+                    <h2>Donation Card</h2>
+                </v-flex>
+            <v-form>
+                <v-text-field label="Donation Title"> </v-text-field>
+            </v-form>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+import { auth, db } from "@/plugins/firebase.js";
+import { mapGetters, mapState } from "vuex";
+import { VStore } from "@/store";
+
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/dcard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <card-form/>
+  </div>
+</template>
+
+<script>
+
+import CardForm from '@/components/DonationCards/CardForm';
+
+export default {
+
+    components: {
+      CardForm
+    },
+
+}
+</script>
+
+<style>
+
+</style>
