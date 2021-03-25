be1162c439b8ac37c0d3d8ec81ace1ed9900c893
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-03-24 18:38:53 -0700

    created a donate button + popup

--- components/Donation/TfuelDialog.vue
+++ components/Donation/TfuelDialog.vue
@@ -1,33 +1,124 @@
 <template>
-    <v-flex xs12 sm10 md8 lg6>
-        <v-card>
-            <!-- Header Bar -->
-            <v-sheet
-                tile
+    <v-card>
+        <!-- Header Bar -->
+        <v-sheet
+            tile
+            color="secondary"
+            class="d-flex align-center pa-2 black--text"
+        >
+            <!-- avatar and name -->
+            <v-avatar color="primary" size="48" class="mr-3">
+                <img :src="avatar" :alt="name" />
+            </v-avatar>
+
+            <h2 class="white--text">{{ name }}</h2>
+        </v-sheet>
+
+        <!-- Donate header -->
+        <h3 class="mt-3 text-center">Donate TFUEL to {{ name }}</h3>
+
+        <!-- Tfuel balance -->
+        <v-card-subtitle class="mt-3">
+            <v-icon color="orange">mdi-arrow-up-bold-box-outline</v-icon>
+            {{ balance }} TFUEL
+        </v-card-subtitle>
+
+        <!-- 1,5,10 tfuel buttons-->
+        <v-layout justify-center>
+            <v-btn-toggle class="mb-3" v-model="donateAmount" borderless>
+                <v-btn value="1" outlined @click="resetCustom">
+                    <v-col>
+                        <v-icon color="orange" class="mb-1"
+                            >mdi-arrow-up-bold-box-outline</v-icon
+                        >
+                        <div>1 TFUEL</div>
+                    </v-col>
+                </v-btn>
+
+                <v-btn value="5" outlined @click="resetCustom">
+                    <v-col>
+                        <v-icon color="orange" class="mb-1"
+                            >mdi-arrow-up-bold-box-outline</v-icon
+                        >
+                        <div>5 TFUEL</div>
+                    </v-col>
+                </v-btn>
+
+                <v-btn value="10" outlined @click="resetCustom">
+                    <v-col>
+                        <v-icon color="orange" class="mb-1"
+                            >mdi-arrow-up-bold-box-outline</v-icon
+                        >
+                        <div>10 TFUEL</div>
+                    </v-col>
+                </v-btn>
+            </v-btn-toggle>
+        </v-layout>
+
+        <!-- custom amount field -->
+        <v-text-field
+            type="number"
+            class="mx-auto mt-2"
+            outlined
+            v-model="customAmount"
+            label="Custom Amount"
+            @input="setDonateAmount"
+        >
+        </v-text-field>
+
+        <!-- governance token info -->
+        <!-- <p class="text-center">
+            You will receive {{ tokenAmount }} {{ tokenName }} for this
+            donation.
+        </p> -->
+        <v-layout justify-center class="my-5">
+            <span> You will receive </span>
+            <v-tooltip top>
+                <template v-slot:activator="{ on, attrs }">
+                    <a href="tokens" v-bind="attrs" v-on="on" class="mx-1">
+                        {{ tokenAmount }} {{ tokenName }}
+                    </a>
+                </template>
+                <span>
+                    This is {{ name }}'s custom token! It is rewarded for
+                    donating TFUEL.</span
+                >
+            </v-tooltip>
+            <span> for this donation. </span>
+        </v-layout>
+
+        <!-- General Success / Error Alert -->
+        <v-alert
+            v-model="alert"
+            class="mt-4 mb-4"
+            :type="alertType"
+            transition="expand-transition"
+            dense
+        >
+            {{ alertMessage }}
+        </v-alert>
+
+        <!-- donate button -->
+        <v-layout justify-center>
+            <v-btn
                 color="secondary"
-                class="d-flex align-center pa-2 black--text"
+                class="white--text mb-3"
+                large
+                :disabled="donateAmount == 0"
+                :loading="donateLoading"
+                @click="onDonate"
             >
-                <!-- replace this with the avatar or something later -->
-                <v-icon class="mr-3" color="orange">mdi-arrow-up-bold-box-outline</v-icon>
+                Donate
+            </v-btn>
+            <v-btn color="grey" plain large @click="closeForm">
+                Cancel
+            </v-btn>
+        </v-layout>
 
-                <h2 class="title"> PROFILE NAME </h2>
-            </v-sheet>
-
-            <!-- Tfuel balance -->
-            <v-icon class="mr-3" color="orange"
-                >circle
-            </v-icon>
-            <span> 0 TFUEL </span>
-
-            <!-- 1,5,10 tfuel buttons-->
-
-            <!-- custom amount field -->
-
-            <!-- governance token info -->
-
-            <!-- donate button -->
-        </v-card>
-    </v-flex>
+        <!-- loader line for extra visual -->
+        <v-progress-linear v-show="lineLoading" indeterminate color="primary">
+        </v-progress-linear>
+    </v-card>
 </template>
 
 <script>
@@ -35,14 +126,84 @@ export default {
     // name export allows it to be usable everywhere
     name: "TfuelDialog",
 
-    // components: {
+    props: {
+        avatar: { type: String },
+        name: { type: String },
+        balance: { type: String },
+        tokenName: { type: String },
+    },
 
-    // }
+    data() {
+        return {
+            donateAmount: 0,
+            customAmount: 0,
+            donateLoading: false,
+            lineLoading: false,
+            alert: false,
+            alertMessage: "Something went wrong!",
+            alertType: "error",
+        };
+    },
+    methods: {
+        // called when donate button is pressed
+        async onDonate() {
+            
+
+            this.donateLoading = true;
+            this.lineLoading = true;
+
+            // do the api stuff here
+            // throw an error if error
+
+            // Success!
+            this.alert = true;
+            this.alertMessage = "Thank you!";
+            this.alertType = "success";
+
+            // stop loading
+            this.donateLoading = false;
+
+            // close the dialog after a little bit
+            setTimeout(() => {
+                this.lineLoading = false;
+                this.closeForm();
+            }, 1000);
+        },
+
+        // reset the form and close it
+        closeForm() {
+            this.donateAmount = 0;
+            this.customAmount = 0;
+            this.alert = false;
+            this.$emit("close");
+        },
+
+        // mirror custom amount to donate amount
+        setDonateAmount(value) {
+            if (isNaN(parseFloat(value))) {
+                this.customAmount = 0;
+            } else {
+                this.customAmount = parseFloat(value);
+            }
+            this.donateAmount = this.customAmount;
+        },
+
+        // reset field of custom amount to 0
+        resetCustom() {
+            this.customAmount = 0;
+        },
+    },
+    computed: {
+        // governance tokens based on amount of tfuel you donate
+        tokenAmount: function () {
+            return this.donateAmount;
+        },
+    },
 };
 </script>
 
-<style>
-.title {
-    color: white;
+<style scoped>
+.v-text-field {
+    max-width: 50%;
 }
 </style>
---
+++ pages/donatetest.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-btn
+        color="primary"
+        class="white--text"
+        small
+        @click="showDonate = true"
+      >
+        Donate
+      </v-btn>
+
+        <!-- make sure this is wrapped this in a v-dialog to do the popup thing -->
+        <v-dialog v-model="showDonate" width="500">
+            <lazy-tfuel-dialog 
+            :avatar="avatar"
+            :name="name"
+            :balance="balance"
+            :tokenName="tokenName"
+            @close="showDonate = false"
+            />
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showDonate: false,
+            avatar: "https://firebasestorage.googleapis.com/v0/b/hark-e2efe.appspot.com/o/WgPFRIKY7WNdADUeOURxuqlLCeT2%2Favatar%2Foutput1.jpg?alt=media&token=49517c31-c936-4c56-8998-fcaafe516856",
+            name: "testman",
+            balance: "100",
+            tokenName: "TEST-HARK",
+        };
+    },
+};
+</script>
+
+<style>
+</style>
